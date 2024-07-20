d77e075d55caf004f7b5eca50448d80f84a71ec0
tom_szpak <tszpak@brandwatch.com>
2024-07-20 22:58:33 +0100

    feat: add map styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import "../features.json";
-import { Map, Marker } from "pigeon-maps";
+import {
+  APIProvider as GMapsProvider,
+  Map,
+  Marker,
+} from "@vis.gl/react-google-maps";
 import {
   Card,
   CardContent,
@@ -8,33 +12,58 @@ import {
   CardHeader,
   CardTitle,
 } from "./components/ui/card";
+import { useState } from "react";
+import mapStyle from "./assets/mapStyle.json";
+
+import "./index.css";
+
 
 export default function App() {
+  const position = { lat: 54.02013940885722, lng: -4.462459976422213 };
+  const [markerPosition, setMarkerPosition] = useState<{lat: number, lng: number} | null>(null);
+
+  const [ viewState, updateViewState] = useState({
+    latitude: position.lat,
+    longitude: position.lng,
+    zoom: 5
+  });
+
+  const mapProps = {
+    options: { styles: mapStyle  }
+  }
+
+  const handleMapClick = (e: any) => {
+    updateViewState(e.map.viewState)      
+    setMarkerPosition(e.detail.latLng)
+  }
+
   return (
-    <div className="flex w-full h-[80vh]">
-      <div className="w-2/3 h-full">
-        {/* put map here */}
-        <Map
-          height={840}
-          width={1000}
-          defaultCenter={[54.02013940885722, -4.462459976422213]}
-          defaultZoom={6}
-        >
-          <Marker width={50} anchor={[50.879, 4.6997]} />
-        </Map>
+    <GMapsProvider apiKey="AIzaSyA92OgNXfxYTHkwqSCvAfy-2U6q70SPfsU">
+      <div className="mainPage__app-container">
+        <div className="mainPage__map_container">
+          <Map 
+            {...mapProps}
+            onClick={(e) => handleMapClick(e)} 
+            viewState={viewState}
+            >
+            <Marker position={markerPosition} />
+          </Map>
+        </div>
+        <div className="mainPage__card-container">
+          <Card>
+            <CardHeader>
+              <CardTitle>Paddling Lake</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Lorem ipsum</p>
+            </CardContent>
+            <CardFooter>
+              <p>Lorem Ipsum</p>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-    </div>
+    </GMapsProvider>
   );
 }
